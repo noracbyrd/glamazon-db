812c038f41743b327d7b93dead23f972978a238d
noracyrd <norabyrd@alumni.upenn.edu>
2019-10-05 00:06:32 -0500

    created node/db connection

--- glamazonCustomer.js
+++ glamazonCustomer.js
@@ -1,3 +1,24 @@
 // requiring npm packages
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+// establishing a connections to the database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "glamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
